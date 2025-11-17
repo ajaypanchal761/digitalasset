7360713333565436d8e459ad7dda5e73b22b1427
hrshiti <hritik45raghuwanshi@gmail.com>
2025-11-17 17:18:36 +0530

    new changes

--- backend/src/routes/health.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getHealthStatus } = require('../controllers/health.controller');
-
-const router = Router();
-
-router.get('/', getHealthStatus);
-
-module.exports = router;
-
